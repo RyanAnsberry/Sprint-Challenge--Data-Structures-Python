974f6086b2a326260ecb8e467a0e5a7d78db2b60
Ryan Ansberry <ansman92@gmail.com>
2020-03-22 14:09:56 -0700

    ring_buffer.py completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,38 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # if length of DLL is less than capacity
+        if len(self.storage) < self.capacity:
+            # add node with item value to tail of DLL
+            self.storage.add_to_tail(item)
+        # if current is None 
+        elif self.current is None:
+            # set current to the head
+            self.current = self.storage.head
+            # assign item to current's value
+            self.current.value = item
+            # set current to next node
+            self.current = self.current.next
+        else:
+            # assign item to current's value
+            self.current.value = item
+            # set current to next node
+            self.current = self.current.next
+            
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        # set a var to iterate through DLL
+        curr_node = self.storage.head
+        # iterate through DLL
+        while curr_node is not None:
+            # append DLL values to list
+            list_buffer_contents.append(curr_node.value)
+            # set curr to next node
+            curr_node = curr_node.next
 
         return list_buffer_contents
 
@@ -30,3 +55,23 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
+buffer = RingBuffer(3)
+
+print(f"Should return [] : {buffer.get()}")
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+print(f"Should return ['a', 'b', 'c'] : {buffer.get()}")
+
+buffer.append('d')
+
+print(f"Should return ['d', 'b', 'c'] : {buffer.get()}")
+
+buffer.append('e')
+buffer.append('f')
+
+print(f"Should return ['d', 'e', 'f'] : {buffer.get()}")
+
